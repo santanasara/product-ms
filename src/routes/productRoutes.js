b74a92ba4086366235efcc7ec5a8455ad44e7252
Sara Beatriz Mendonça de Santana <sarabeatriz@ufrn.edu.br>
2021-03-17 08:23:00 -0300

    Making some changes to run the project

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -9,15 +9,14 @@ import {
   createProductReview,
   getTopProducts,
 } from '../controllers/productController.js'
-import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').get(getProducts).post(protect, admin, createProduct)
-router.route('/:id/reviews').post(protect, createProductReview)
+router.route('/').get(getProducts).post(createProduct)
+router.route('/:id/reviews').post(createProductReview)
 router.get('/top', getTopProducts)
 router
   .route('/:id')
   .get(getProductById)
-  .delete(protect, admin, deleteProduct)
-  .put(protect, admin, updateProduct)
+  .delete(deleteProduct)
+  .put(updateProduct)
 
 export default router
