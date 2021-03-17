7da511df11647d6da149bcadcd33b94268cc6e82
Sara Beatriz Mendonça de Santana <sarabeatriz@ufrn.edu.br>
2021-03-17 08:29:51 -0300

    Remove mode from server message

--- src/index.js
+++ src/index.js
@@ -25,6 +25,6 @@ const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server running mode on port ${PORT}`
   )
 )
