e0b8461187350de4f4d58e8328eeb4a754495ba6
Sara Beatriz Mendonça de Santana <sarabeatriz@ufrn.edu.br>
2021-03-27 20:45:33 -0300

    Creates protect and admin endpoints and deletes middleware functions

--- authentication/src/controllers/authController.js
+++ authentication/src/controllers/authController.js
@@ -186,6 +186,43 @@ const updateUser = asyncHandler(async (req, res) => {
   }
 });
 
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+const admin = asyncHandler((req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+});
+
 export {
   validateUser,
   authUser,
@@ -196,4 +233,6 @@ export {
   deleteUser,
   getUserById,
   updateUser,
+  admin,
+  protect,
 };
